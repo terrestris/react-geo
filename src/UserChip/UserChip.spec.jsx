bb697f7db24f0946b8666c7585a29dc9201ef55b
Andre Henn <henn@terrestris.de>
2017-09-01 16:50:57 +0200

    adds user chip and tests

---
+++ src/UserChip/UserChip.spec.jsx
@@ -0,0 +1,48 @@
+/*eslint-env mocha*/
+import expect from 'expect.js';
+import UserChip from './UserChip.jsx';
+import testImage from  './user.png';
+import TestUtils from '../Util/TestUtils';
+
+describe('<UserChip />', () => {
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = TestUtils.mountComponent(UserChip);
+  });
+
+  it('is defined', () => {
+    expect(UserChip).not.to.be(undefined);
+  });
+
+  it('can be rendered', () => {
+    expect(wrapper).not.to.be(undefined);
+  });
+
+  it('determines initials from given user name', () => {
+    wrapper.setProps({userName: 'Shinji Kagawa'});
+    let test = wrapper.instance().getInitials();
+    expect(test).to.be('SK');
+  });
+
+  it('uses imageSrc if image is given', () => {
+    const props = {
+      imageSrc: testImage
+    };
+    const wrapper =TestUtils.mountComponent(UserChip, props);
+    expect(wrapper.find('img').node.src).to.contain(testImage);
+  });
+
+  it('uses initials if image is not given', () => {
+    wrapper.setProps({userName: 'Shinji Kagawa'});
+    expect(wrapper.find('img').node).to.be(undefined);
+  });
+
+  it('should not render a dropdown for invalid configuration', () => {
+    wrapper.setProps({userName: 'Shinji Kagawa'});
+    wrapper.setProps({userMenu: null});
+    expect(wrapper.find('ul.user-appinfo-menu').node).to.be(undefined);
+  });
+
+});
