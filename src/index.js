2251ee76dc5d24c455a73831c893d0c10ac325f3
Kai Volland <kaivolland@yahoo.de>
2017-09-06 14:20:29 +0200

    Use SimpleButton in Toolbar example

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import {UserChip} from './UserChip/UserChip.jsx';
-import {Toolbar} from './Toolbar/Toolbar.jsx';
+import UserChip from './UserChip/UserChip.jsx';
+import Toolbar from './Toolbar/Toolbar.jsx';
 import SimpleButton from './Button/SimpleButton/SimpleButton.jsx';
 import ToggleButton from './Button/ToggleButton/ToggleButton.jsx';
 import ToggleGroup from './Button/ToggleGroup/ToggleGroup.jsx';
