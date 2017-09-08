d11835974c3bcca2d52530a0fda50033e54f50cb
Kai Volland <kaivolland@yahoo.de>
2017-09-08 13:38:01 +0200

    Fix coverage config

--- karma.conf.js
+++ karma.conf.js
@@ -22,6 +22,7 @@ module.exports = function(config) {
 
     // list of files to exclude
     exclude: [
+      '**/TestUtils.js'
     ],
 
     // preprocess matching files before serving them to the browser
