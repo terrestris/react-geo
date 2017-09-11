cde812181ab9c8d49a95310491796675ccdd54bc
Daniel Koch <koch@terrestris.de>
2017-09-11 11:44:03 +0000

    Update distribution resources on master branch
    This commit was autogenerated by the 'update-dist-folder.sh' script.

--- dist/index.js
+++ dist/index.js
@@ -3,15 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.isVisibleComponent = exports.UndoUtil = exports.ObjectUtil = exports.MapUtil = exports.Logger = exports.CsrfUtil = exports.ScaleCombo = exports.FloatingMapLogo = exports.Toolbar = exports.UserChip = exports.ToggleGroup = exports.ToggleButton = exports.SimpleButton = undefined;
-
-var _UserChip = require('./UserChip/UserChip.js');
-
-var _UserChip2 = _interopRequireDefault(_UserChip);
-
-var _Toolbar = require('./Toolbar/Toolbar.js');
-
-var _Toolbar2 = _interopRequireDefault(_Toolbar);
+exports.isVisibleComponent = exports.UndoUtil = exports.StringUtil = exports.ObjectUtil = exports.MapUtil = exports.Logger = exports.FeatureUtil = exports.CsrfUtil = exports.UserChip = exports.Toolbar = exports.Titlebar = exports.Panel = exports.ScaleCombo = exports.FloatingMapLogo = exports.LayerTree = exports.ToggleGroup = exports.ToggleButton = exports.SimpleButton = undefined;
 
 var _SimpleButton = require('./Button/SimpleButton/SimpleButton.js');
 
@@ -25,6 +17,10 @@ var _ToggleGroup = require('./Button/ToggleGroup/ToggleGroup.js');
 
 var _ToggleGroup2 = _interopRequireDefault(_ToggleGroup);
 
+var _LayerTree = require('./LayerTree/LayerTree.js');
+
+var _LayerTree2 = _interopRequireDefault(_LayerTree);
+
 var _FloatingMapLogo = require('./Map/FloatingMapLogo/FloatingMapLogo.js');
 
 var _FloatingMapLogo2 = _interopRequireDefault(_FloatingMapLogo);
@@ -33,9 +29,29 @@ var _ScaleCombo = require('./Map/ScaleCombo/ScaleCombo.js');
 
 var _ScaleCombo2 = _interopRequireDefault(_ScaleCombo);
 
+var _Panel = require('./Panel/Panel/Panel.js');
+
+var _Panel2 = _interopRequireDefault(_Panel);
+
+var _Titlebar = require('./Panel/Titlebar/Titlebar.js');
+
+var _Titlebar2 = _interopRequireDefault(_Titlebar);
+
+var _Toolbar = require('./Toolbar/Toolbar.js');
+
+var _Toolbar2 = _interopRequireDefault(_Toolbar);
+
+var _UserChip = require('./UserChip/UserChip.js');
+
+var _UserChip2 = _interopRequireDefault(_UserChip);
+
 var _CsrfUtil = require('./Util/CsrfUtil.js');
 
 var _CsrfUtil2 = _interopRequireDefault(_CsrfUtil);
+
+var _FeatureUtil = require('./Util/FeatureUtil.js');
+
+var _FeatureUtil2 = _interopRequireDefault(_FeatureUtil);
 
 var _Logger = require('./Util/Logger.js');
 
@@ -49,6 +65,10 @@ var _ObjectUtil = require('./Util/ObjectUtil.js');
 
 var _ObjectUtil2 = _interopRequireDefault(_ObjectUtil);
 
+var _StringUtil = require('./Util/StringUtil.js');
+
+var _StringUtil2 = _interopRequireDefault(_StringUtil);
+
 var _UndoUtil = require('./Util/UndoUtil.js');
 
 var _UndoUtil2 = _interopRequireDefault(_UndoUtil);
@@ -60,13 +80,18 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 exports.SimpleButton = _SimpleButton2.default;
 exports.ToggleButton = _ToggleButton2.default;
 exports.ToggleGroup = _ToggleGroup2.default;
-exports.UserChip = _UserChip2.default;
-exports.Toolbar = _Toolbar2.default;
+exports.LayerTree = _LayerTree2.default;
 exports.FloatingMapLogo = _FloatingMapLogo2.default;
 exports.ScaleCombo = _ScaleCombo2.default;
+exports.Panel = _Panel2.default;
+exports.Titlebar = _Titlebar2.default;
+exports.Toolbar = _Toolbar2.default;
+exports.UserChip = _UserChip2.default;
 exports.CsrfUtil = _CsrfUtil2.default;
+exports.FeatureUtil = _FeatureUtil2.default;
 exports.Logger = _Logger2.default;
 exports.MapUtil = _MapUtil2.default;
 exports.ObjectUtil = _ObjectUtil2.default;
+exports.StringUtil = _StringUtil2.default;
 exports.UndoUtil = _UndoUtil2.default;
 exports.isVisibleComponent = _VisibleComponent.isVisibleComponent;
