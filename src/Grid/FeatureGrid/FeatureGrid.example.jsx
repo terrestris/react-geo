a63c2e041f9c7df33164b22374eed8181b8f0814
Andre Henn <henn@terrestris.de>
2017-11-16 17:14:03 +0100

    adds examples for custom column widths and column name mappings

--- src/Grid/FeatureGrid/FeatureGrid.example.jsx
+++ src/Grid/FeatureGrid/FeatureGrid.example.jsx
@@ -25,15 +25,43 @@ const attributeObject = {
 
 feature.setProperties(attributeObject);
 feature.setId(1909);
+
+const attributeNames = {
+  foo: 'A',
+  foo2: 'nice',
+  foo9: 'example'
+};
 //
 // ***************************** SETUP END *************************************
 //
 
 render(
   <div className="example-block">
-    <span>FeatureGrid:</span>
+    <h2>FeatureGrid without a filter:</h2>
     <FeatureGrid
       feature={feature}
+    />
+    <br />
+    <h2>FeatureGrid with filtered attributes (foo and foo9 only):</h2>
+    <FeatureGrid
+      feature={feature}
+      attributeFilter={['foo', 'foo9']}
+      style={{width: '50%'}}
+    />
+    <br />
+    <h2>FeatureGrid with different column width (70 % width for column name / 30 % width for column value):</h2>
+    <FeatureGrid
+      feature={feature}
+      attributeNameColumnWidthInPercent={70}
+      style={{width: '50%'}}
+    />
+    <br />
+    <h2>FeatureGrid with column name mapping:</h2>
+    <FeatureGrid
+      feature={feature}
+      attributeFilter={['foo', 'foo2', 'foo9', 'name']}
+      attributeNames={attributeNames}
+      style={{width: '50%'}}
     />
   </div>,
   document.getElementById('exampleContainer')
