22c69eef010d46155514e8238fa3a1299b336624
annarieger <rieger@terrestris.de>
2017-12-04 09:59:35 +0000

    Update resources on gh-pages branch
    This commit was autogenerated by the 'update-gh-pages.sh' script.

---
+++ examples/Button/MeasureButton/MeasureButton.example.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import OlMap from 'ol/map';
+import OlView from 'ol/view';
+import OlLayerTile from 'ol/layer/tile';
+import OlSourceOsm from 'ol/source/osm';
+import olProj from 'ol/proj';
+
+import {
+  MeasureButton
+} from '../../index.js';
+
+//
+// ***************************** SETUP *****************************************
+//
+const map = new OlMap({
+  layers: [
+    new OlLayerTile({
+      name: 'OSM',
+      source: new OlSourceOsm()
+    })
+  ],
+  view: new OlView({
+    center: olProj.fromLonLat([37.40570, 8.81566]),
+    zoom: 4
+  })
+});
+
+//
+// ***************************** SETUP END *************************************
+//
+
+render(
+  <div>
+    <div id="map" style={{
+      width: '800px',
+      height: '400px'
+    }} />
+
+    <div className="example-block">
+      <span>Measure type:</span>
+      <MeasureButton
+        map={map}
+        measureType="line"
+      >
+        Distance
+      </MeasureButton>
+
+      <MeasureButton
+        map={map}
+        measureType="line"
+        showMeasureInfoOnClickedPoints
+      >
+        Distance with step labels
+      </MeasureButton>
+
+      <MeasureButton
+        map={map}
+        measureType="line"
+        multipleDrawing
+      >
+        Distance with multiple drawing
+      </MeasureButton>
+
+      <MeasureButton
+        map={map}
+        measureType="polygon"
+      >
+        Area
+      </MeasureButton>
+
+      <MeasureButton
+        map={map}
+        measureType="angle"
+      >
+        Angle
+      </MeasureButton>
+
+    </div>
+
+  </div>,
+
+  // Target
+  document.getElementById('exampleContainer'),
+
+  // Callback
+  () => {
+    map.setTarget('map');
+  }
+);
