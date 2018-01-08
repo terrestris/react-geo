04eb0bc9d609debe2586170cc93085ebdb66ecd7
Marc Jansen <jansen@terrestris.de>
2018-01-08 14:39:55 +0100

    Add tests for MapProvider, thx @dnlkoch

--- src/HigherOrderComponent/MapProvider/MapProvider.spec.jsx
+++ src/HigherOrderComponent/MapProvider/MapProvider.spec.jsx
@@ -1,47 +1,67 @@
 /*eslint-env jest*/
-// import React from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-// import { mount } from 'enzyme';
-//
-// import TestUtil from '../../Util/TestUtil';
+import { mount } from 'enzyme';
+
+import { TestUtil } from '../../Util/TestUtil';
 
 import {
-  MapProvider
+  MapProvider,
+  mappify
 } from '../../index';
 
 describe('MapProvider', () => {
-  // let map;
-  //
-  // /* eslint-disable require-jsdoc */
-  // class MockComponent extends React.Component {
-  //   render() {
-  //     return (
-  //       <div>A mock Component</div>
-  //     );
-  //   }
-  // }
+  /* eslint-disable require-jsdoc */
+  class MockComponent extends React.Component {
+    static propTypes = {
+      map: PropTypes.object.isRequired,
+    }
+    render() {
+      return (
+        <div>Mockety-mock</div>
+      );
+    }
+  }
   /* eslint-enable require-jsdoc */
-
-  // beforeEach(() => {
-  //   map = TestUtil.createMap();
-  // });
+  const MappifiedMockComponent = mappify(MockComponent);
 
   describe('Basics', () => {
     it('is defined', () => {
       expect(MapProvider).not.toBeUndefined();
     });
 
-    // TODO
-    // it('provides a given map to its children', () => {
-    //   const wrapper = mount(
-    //     <MapProvider map={map}>
-    //       <MockComponent />
-    //     </MapProvider>
-    //   );
-    //
-    //   expect(wrapper).not.toBeUndefined();
-    //   expect(wrapper.context().map).toBemap);
-    // });
+    it('provides a given map to its children (ol.Map)', () => {
+      const map = TestUtil.createMap();
+      const wrapper = mount(
+        <MapProvider map={map}>
+          <MappifiedMockComponent />
+        </MapProvider>
+      );
+      const isReady = wrapper.state('ready');
+      expect(isReady).toBe(true);
 
+      const mapFromMock = wrapper.find(MockComponent).prop('map');
+      expect(mapFromMock).toBe(map);
+    });
+
+    it('provides a given map to its children (Promise)', () => {
+      const map = TestUtil.createMap();
+      const mapPromise = new Promise((resolve) => {
+        resolve(map);
+      });
+      const wrapper = mount(
+        <MapProvider map={mapPromise}>
+          <MappifiedMockComponent />
+        </MapProvider>
+      );
+      return mapPromise.then(() => {
+        const isReady = wrapper.state('ready');
+        expect(isReady).toBe(true);
+        wrapper.update();
+        const mapFromMock = wrapper.find(MockComponent).prop('map');
+        expect(mapFromMock).toBe(map);
+      });
+    });
   });
 });
