158e0b5e41a1f471c41a9f44d5eddacf98d1d346
Marc Jansen <jansen@terrestris.de>
2018-01-29 12:25:43 +0100

    Simplify the code of map provider

--- src/HigherOrderComponent/MapProvider/MapProvider.spec.jsx
+++ src/HigherOrderComponent/MapProvider/MapProvider.spec.jsx
@@ -33,16 +33,32 @@ describe('MapProvider', () => {
 
     it('provides a given map to its children (ol.Map)', () => {
       const map = TestUtil.createMap();
+      // We create the promise only to be able to return it below, we do not
+      // actually use it to instantiate MapProvider
+      const mapPromise = new Promise((resolve) => {
+        resolve(map);
+      });
+
       const wrapper = mount(
-        <MapProvider map={map}>
+        <MapProvider map={map}> {/* See, we use the map, not the promise*/}
           <MappifiedMockComponent />
         </MapProvider>
       );
-      const isReady = wrapper.state('ready');
-      expect(isReady).toBe(true);
 
-      const mapFromMock = wrapper.find(MockComponent).prop('map');
-      expect(mapFromMock).toBe(map);
+      // resolve our promise, so this async behaviour is testable.
+      setTimeout(() => {
+        mapPromise.resolve(map);
+      }, 50);
+
+      expect.assertions(2);
+      return mapPromise.then(() => {
+        const isReady = wrapper.state('ready');
+        expect(isReady).toBe(true);
+        wrapper.update();
+        const mapFromMock = wrapper.find(MockComponent).prop('map');
+        expect(mapFromMock).toBe(map);
+      });
+
     });
 
     it('provides a given map to its children (Promise)', () => {
@@ -51,10 +67,11 @@ describe('MapProvider', () => {
         resolve(map);
       });
       const wrapper = mount(
-        <MapProvider map={mapPromise}>
+        <MapProvider map={mapPromise}> {/* See, we now use the actual promise*/}
           <MappifiedMockComponent />
         </MapProvider>
       );
+      expect.assertions(2);
       return mapPromise.then(() => {
         const isReady = wrapper.state('ready');
         expect(isReady).toBe(true);
