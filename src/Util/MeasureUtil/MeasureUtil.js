6bdab6b3714198157a2f32154551853e9d3ac232
Marc Jansen <jansen@terrestris.de>
2018-03-19 11:19:45 +0100

    MeasureUtil: arrow functions to class methods and eslint

--- src/Util/MeasureUtil/MeasureUtil.js
+++ src/Util/MeasureUtil/MeasureUtil.js
@@ -9,192 +9,192 @@ import OlProj from 'ol/proj';
  */
 class MeasureUtil {
 
-    /**
-     * Format length output for the tooltip.
-     *
-     * @param {OlGeomMultiLineString} line The drawn line.
-     * @param {OlMap} map An OlMap.
-     * @param {Number} decimalPlacesInToolTips How many decimal places will be
-     *   allowed for the measure tooltips
-     * @param {Boolean} geodesic Is the measurement geodesic (default is true).
-     *
-     * @return {String} The formatted length of the line.
-     */
-    static formatLength = (line, map, decimalPlacesInToolTips, geodesic = true) => {
-      const decimalHelper = Math.pow(10, decimalPlacesInToolTips);
-      let length;
+  /**
+   * Format length output for the tooltip.
+   *
+   * @param {OlGeomMultiLineString} line The drawn line.
+   * @param {OlMap} map An OlMap.
+   * @param {Number} decimalPlacesInToolTips How many decimal places will be
+   *   allowed for the measure tooltips
+   * @param {Boolean} geodesic Is the measurement geodesic (default is true).
+   *
+   * @return {String} The formatted length of the line.
+   */
+  static formatLength(line, map, decimalPlacesInToolTips, geodesic = true) {
+    const decimalHelper = Math.pow(10, decimalPlacesInToolTips);
+    let length;
 
-      if (geodesic) {
-        const wgs84Sphere = new OlSphere(6378137);
-        const coordinates = line.getCoordinates();
-        length = 0;
-        const sourceProj = map.getView().getProjection();
-        for (let i = 0, ii = coordinates.length - 1; i < ii; ++i) {
-          const c1 = OlProj.transform(
-            coordinates[i], sourceProj, 'EPSG:4326');
-          const c2 = OlProj.transform(
-            coordinates[i + 1], sourceProj, 'EPSG:4326');
-          length += wgs84Sphere.haversineDistance(c1, c2);
-        }
-      } else {
-        length = Math.round(line.getLength() * 100) / 100;
+    if (geodesic) {
+      const wgs84Sphere = new OlSphere(6378137);
+      const coordinates = line.getCoordinates();
+      length = 0;
+      const sourceProj = map.getView().getProjection();
+      for (let i = 0, ii = coordinates.length - 1; i < ii; ++i) {
+        const c1 = OlProj.transform(
+          coordinates[i], sourceProj, 'EPSG:4326');
+        const c2 = OlProj.transform(
+          coordinates[i + 1], sourceProj, 'EPSG:4326');
+        length += wgs84Sphere.haversineDistance(c1, c2);
       }
+    } else {
+      length = Math.round(line.getLength() * 100) / 100;
+    }
 
-      let output;
-      if (length > 1000) {
-        output = (Math.round(length / 1000 * decimalHelper) /
+    let output;
+    if (length > 1000) {
+      output = (Math.round(length / 1000 * decimalHelper) /
                 decimalHelper) + ' ' + 'km';
-      } else {
-        output = (Math.round(length * decimalHelper) / decimalHelper) +
+    } else {
+      output = (Math.round(length * decimalHelper) / decimalHelper) +
                 ' m';
-      }
-      return output;
+    }
+    return output;
+  }
+
+  /**
+   * Format length output for the tooltip.
+   *
+   * @param {OlGeomPolygon} polygon The drawn polygon.
+   * @param {OlMap} map An OlMap.
+   * @param {Number} decimalPlacesInToolTips How many decimal places will be
+   *   allowed for the measure tooltips.
+   * @param {Boolean} geodesic Is the measurement geodesic.
+   *
+   * @return {String} The formatted area of the polygon.
+   */
+  static formatArea(polygon, map, decimalPlacesInToolTips, geodesic = true) {
+    const decimalHelper = Math.pow(10, decimalPlacesInToolTips);
+    let area;
+
+    if (geodesic) {
+      const wgs84Sphere = new OlSphere(6378137);
+      const sourceProj = map.getView().getProjection();
+      const geom = (polygon.clone().transform(
+        sourceProj, 'EPSG:4326'));
+      const coordinates = geom.getLinearRing(0).getCoordinates();
+      area = Math.abs(wgs84Sphere.geodesicArea(coordinates));
+    } else {
+      area = polygon.getArea();
     }
 
-    /**
-     * Format length output for the tooltip.
-     *
-     * @param {OlGeomPolygon} polygon The drawn polygon.
-     * @param {OlMap} map An OlMap.
-     * @param {Number} decimalPlacesInToolTips How many decimal places will be
-     *   allowed for the measure tooltips.
-     * @param {Boolean} geodesic Is the measurement geodesic.
-     *
-     * @return {String} The formatted area of the polygon.
-     */
-    static formatArea = (polygon, map, decimalPlacesInToolTips, geodesic = true) => {
-      const decimalHelper = Math.pow(10, decimalPlacesInToolTips);
-      let area;
-
-      if (geodesic) {
-        const wgs84Sphere = new OlSphere(6378137);
-        const sourceProj = map.getView().getProjection();
-        const geom = (polygon.clone().transform(
-          sourceProj, 'EPSG:4326'));
-        const coordinates = geom.getLinearRing(0).getCoordinates();
-        area = Math.abs(wgs84Sphere.geodesicArea(coordinates));
-      } else {
-        area = polygon.getArea();
-      }
-
-      let output;
-      if (area > 10000) {
-        output = (Math.round(area / 1000000 * decimalHelper) /
+    let output;
+    if (area > 10000) {
+      output = (Math.round(area / 1000000 * decimalHelper) /
                 decimalHelper) + ' km<sup>2</sup>';
-      } else {
-        output = (Math.round(area * decimalHelper) / decimalHelper) +
+    } else {
+      output = (Math.round(area * decimalHelper) / decimalHelper) +
                 ' ' + 'm<sup>2</sup>';
-      }
-      return output;
+    }
+    return output;
+  }
+
+  /**
+   * Determine the angle between two coordinates. The angle will be between
+   * -180° and 180°, with 0° being in the east. The angle will increase
+   * counter-clockwise.
+   *
+   * Inspired by http://stackoverflow.com/a/31136507
+   *
+   * @param {Array<Number>} start The start coordinates of the line with the
+   *     x-coordinate being at index `0` and y-coordinate being at index `1`.
+   * @param {Array<Number>} end The end coordinates of the line with the
+   *     x-coordinate being at index `0` and y-coordinate being at index `1`.
+   *
+   * @return {Number} the angle in degreees, ranging from -180° to 180°.
+   */
+  static angle(start, end) {
+    const dx = start[0] - end[0];
+    const dy = start[1] - end[1];
+    // range (-PI, PI]
+    let theta = Math.atan2(dy, dx);
+    // rads to degs, range (-180, 180]
+    theta *= 180 / Math.PI;
+    return theta;
+  }
+
+  /**
+   * Determine the angle between two coordinates. The angle will be between
+   * 0° and 360°, with 0° being in the east. The angle will increase
+   * counter-clockwise.
+   *
+   * Inspired by http://stackoverflow.com/a/31136507
+   *
+   * @param {Array<Number>} start The start coordinates of the line with the
+   *     x-coordinate being at index `0` and y-coordinate being at index `1`.
+   * @param {Array<Number>} end The end coordinates of the line with the
+   *     x-coordinate being at index `0` and y-coordinate being at index `1`.
+   *
+   * @return {Number} the angle in degrees, ranging from 0° and 360°.
+   */
+  static angle360(start, end) {
+    // range (-180, 180]
+    let theta = MeasureUtil.angle(start, end);
+    if (theta < 0) {
+      // range [0, 360)
+      theta = 360 + theta;
+    }
+    return theta;
+  }
+
+  /**
+   * Given an angle between 0° and 360° this angle returns the exact opposite
+   * of the angle, e.g. for 90° you'll get back 270°. This effectively turns
+   * the direction of the angle from counter-clockwise to clockwise.
+   *
+   * @param {Number} angle360 The input angle obtained counter-clockwise.
+   *
+   * @return {Number} The clockwise angle.
+   */
+  static makeClockwise(angle360) {
+    return 360 - angle360;
+  }
+
+  /**
+   * This methods adds an offset of 90° to an counter-clockwise increasing
+   * angle of a line so that the origin (0°) lies at the top (in the north).
+   *
+   * @param {Number} angle360 The input angle obtained counter-clockwise, with
+   *     0° degrees being in the east.
+   *
+   * @return {Number} The adjusted angle, with 0° being in the north.
+   */
+  static makeZeroDegreesAtNorth(angle360) {
+    let corrected = angle360 + 90;
+    if (corrected > 360) {
+      corrected = corrected - 360;
+    }
+    return corrected;
+  }
+
+  /**
+   * Returns the angle of the passed linestring in degrees, with 'N' being the
+   * 0°-line and the angle increases in clockwise direction.
+   *
+   * @param {OlGeomLineString} line The linestring to get the
+   *   angle from. As this line is comming from our internal draw
+   *   interaction, we know that it will only consist of two points.
+   * @param {Number} decimalPlacesInToolTips How many decimal places will be
+   *   allowed for the measure tooltips.
+   *
+   * @return {String} The formatted angle of the line.
+   */
+  static formatAngle(line, decimalPlacesInToolTips = 2) {
+    const coords = line.getCoordinates();
+    const numCoords = coords.length;
+    if (numCoords < 2) {
+      return '';
     }
 
-    /**
-     * Determine the angle between two coordinates. The angle will be between
-     * -180° and 180°, with 0° being in the east. The angle will increase
-     * counter-clockwise.
-     *
-     * Inspired by http://stackoverflow.com/a/31136507
-     *
-     * @param {Array<Number>} start The start coordinates of the line with the
-     *     x-coordinate being at index `0` and y-coordinate being at index `1`.
-     * @param {Array<Number>} end The end coordinates of the line with the
-     *     x-coordinate being at index `0` and y-coordinate being at index `1`.
-     *
-     * @return {Number} the angle in degreees, ranging from -180° to 180°.
-     */
-    static angle = (start, end) => {
-      const dx = start[0] - end[0];
-      const dy = start[1] - end[1];
-      // range (-PI, PI]
-      let theta = Math.atan2(dy, dx);
-      // rads to degs, range (-180, 180]
-      theta *= 180 / Math.PI;
-      return theta;
-    }
+    const lastPoint = coords[numCoords - 1];
+    const prevPoint = coords[numCoords - 2];
+    let angle = MeasureUtil.angle360(prevPoint, lastPoint);
 
-    /**
-     * Determine the angle between two coordinates. The angle will be between
-     * 0° and 360°, with 0° being in the east. The angle will increase
-     * counter-clockwise.
-     *
-     * Inspired by http://stackoverflow.com/a/31136507
-     *
-     * @param {Array<Number>} start The start coordinates of the line with the
-     *     x-coordinate being at index `0` and y-coordinate being at index `1`.
-     * @param {Array<Number>} end The end coordinates of the line with the
-     *     x-coordinate being at index `0` and y-coordinate being at index `1`.
-     *
-     * @return {Number} the angle in degrees, ranging from 0° and 360°.
-     */
-    static angle360 = (start, end) => {
-      // range (-180, 180]
-      let theta = MeasureUtil.angle(start, end);
-      if (theta < 0) {
-        // range [0, 360)
-        theta = 360 + theta;
-      }
-      return theta;
-    }
+    angle = MeasureUtil.makeZeroDegreesAtNorth(angle);
+    angle = MeasureUtil.makeClockwise(angle);
+    angle = angle.toFixed(decimalPlacesInToolTips);
 
-    /**
-     * Given an angle between 0° and 360° this angle returns the exact opposite
-     * of the angle, e.g. for 90° you'll get back 270°. This effectively turns
-     * the direction of the angle from counter-clockwise to clockwise.
-     *
-     * @param {Number} angle360 The input angle obtained counter-clockwise.
-     *
-     * @return {Number} The clockwise angle.
-     */
-    static makeClockwise = (angle360) => {
-      return 360 - angle360;
-    }
-
-    /**
-     * This methods adds an offset of 90° to an counter-clockwise increasing
-     * angle of a line so that the origin (0°) lies at the top (in the north).
-     *
-     * @param {Number} angle360 The input angle obtained counter-clockwise, with
-     *     0° degrees being in the east.
-     *
-     * @return {Number} The adjusted angle, with 0° being in the north.
-     */
-    static makeZeroDegreesAtNorth = (angle360) => {
-      let corrected = angle360 + 90;
-      if (corrected > 360) {
-        corrected = corrected - 360;
-      }
-      return corrected;
-    }
-
-    /**
-     * Returns the angle of the passed linestring in degrees, with 'N' being the
-     * 0°-line and the angle increases in clockwise direction.
-     *
-     * @param {OlGeomLineString} line The linestring to get the
-     *   angle from. As this line is comming from our internal draw
-     *   interaction, we know that it will only consist of two points.
-     * @param {Number} decimalPlacesInToolTips How many decimal places will be
-     *   allowed for the measure tooltips.
-     *
-     * @return {String} The formatted angle of the line.
-     */
-    static formatAngle = (line, decimalPlacesInToolTips = 2) => {
-      const coords = line.getCoordinates();
-      const numCoords = coords.length;
-      if (numCoords < 2) {
-        return '';
-      }
-
-      const lastPoint = coords[numCoords - 1];
-      const prevPoint = coords[numCoords - 2];
-      let angle = MeasureUtil.angle360(prevPoint, lastPoint);
-
-      angle = MeasureUtil.makeZeroDegreesAtNorth(angle);
-      angle = MeasureUtil.makeClockwise(angle);
-      angle = angle.toFixed(decimalPlacesInToolTips);
-
-      return angle + '°';
-    }
+    return angle + '°';
+  }
 
 }
 
