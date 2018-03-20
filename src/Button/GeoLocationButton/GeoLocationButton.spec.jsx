fc182197af92a1bacfbe46b96f50b5539d491a1d
Andreas Schmitz <schmitz@terrestris.de>
2018-03-20 15:10:24 +0100

    added a couple more tests

--- src/Button/GeoLocationButton/GeoLocationButton.spec.jsx
+++ src/Button/GeoLocationButton/GeoLocationButton.spec.jsx
@@ -24,5 +24,35 @@ describe('<GeoLocationButton />', () => {
       expect(wrapper).not.toBeUndefined();
     });
 
+    it('creates the geolocation interaction on the fly', function() {
+      const wrapper = TestUtil.mountComponent(GeoLocationButton, {
+        map: map,
+        showMarker: false
+      });
+      const instance = wrapper.instance();
+      expect(instance.geolocationInteraction).toBeUndefined();
+    });
+
+    it('can be pressed', () => {
+      const wrapper = TestUtil.mountComponent(GeoLocationButton, {
+        map: map,
+        showMarker: false
+      });
+      const instance = wrapper.instance();
+      instance.onToggle(true);
+      expect(instance.geolocationInteraction).not.toBeUndefined();
+    });
+
+    it('can be pressed twice', () => {
+      const wrapper = TestUtil.mountComponent(GeoLocationButton, {
+        map: map,
+        showMarker: false
+      });
+      const instance = wrapper.instance();
+      instance.onToggle(true);
+      instance.onToggle(false);
+      expect(instance.geolocationInteraction).toBeNull();
+    });
+
   });
 });
