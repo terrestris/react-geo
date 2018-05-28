62ddbeaca6fe2fd1f8449adee592ca43f061d2f3
abaghdoust <baghdoust@terrestris.de>
2018-05-28 13:37:34 +0200

    Fixes indentations

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -1,44 +1,40 @@
 const path = require('path');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   module: {
     rules: [{
-        test: /\.jsx?$/,
-         exclude: /node_modules/,
-        use: 'babel-loader'
-      }, {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }, {
-        test: /\.less$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'less-loader',
-            options: {
-              javascriptEnabled: true
-            }
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: 'babel-loader'
+    }, {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    }, {
+      test: /\.less$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'less-loader',
+          options: {
+            javascriptEnabled: true
           }
-        ]
-      }, {
-        test: /\.(jpe?g|png|gif|ico)$/i,
-        use: [
-          'file-loader?name=img/[name].[ext]'
-        ]
-      }, {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'url-loader?limit=10000&mimetype=application/font-woff'
-      }, {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'file-loader'
-      }]
-  },
-  plugins: [
-    new BundleAnalyzerPlugin()
-  ]
+        }
+      ]
+    }, {
+      test: /\.(jpe?g|png|gif|ico)$/i,
+      use: [
+        'file-loader?name=img/[name].[ext]'
+      ]
+    }, {
+      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      use: 'url-loader?limit=10000&mimetype=application/font-woff'
+    }, {
+      test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      use: 'file-loader'
+    }]
+  }
 };
