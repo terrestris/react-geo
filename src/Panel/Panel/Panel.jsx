803a842e0b2093e7bef57ad43ef39ab30f9e4c2b
Daniel Koch <koch@terrestris.de>
2018-11-19 15:31:17 +0100

    Fix import of Rnd

--- src/Panel/Panel/Panel.jsx
+++ src/Panel/Panel/Panel.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Rnd from 'react-rnd';
+import { Rnd } from 'react-rnd';
 
 import uniqueId from 'lodash/uniqueId';
 import isNumber from 'lodash/isNumber';
