afebff2a70a0c8222415952cf9da5a9c380c3eb1
Daniel Koch <koch@terrestris.de>
2019-06-19 13:54:31 +0200

    Make use of xmldom and jest-canvas-mock in jest setup

--- jest/setup.js
+++ jest/setup.js
@@ -1,5 +1,9 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { XMLSerializer } from 'xmldom';
 import 'whatwg-fetch';
+import 'jest-canvas-mock';
+
+global.XMLSerializer = XMLSerializer;
 
 Enzyme.configure({ adapter: new Adapter() });
