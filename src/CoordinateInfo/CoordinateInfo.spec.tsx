c4fa805ddb7a98ee48d2c8def9dcf0a9c0a2097c
Daniel Koch <koch@terrestris.de>
2020-02-12 18:31:39 +0100

    Pass required prop

--- src/CoordinateInfo/CoordinateInfo.spec.tsx
+++ src/CoordinateInfo/CoordinateInfo.spec.tsx
@@ -5,12 +5,18 @@ import TestUtil from '../Util/TestUtil';
 import CoordinateInfo from '../CoordinateInfo/CoordinateInfo';
 
 describe('<CoordinateInfo />', () => {
+  let map;
+
+  beforeEach(() => {
+    map = TestUtil.createMap();
+  });
+
   it('is defined', () => {
     expect(CoordinateInfo).not.toBeUndefined();
   });
 
   it('can be rendered', () => {
-    const wrapper = TestUtil.mountComponent(CoordinateInfo);
+    const wrapper = TestUtil.mountComponent(CoordinateInfo, {map});
     expect(wrapper).not.toBeUndefined();
   });
 });
