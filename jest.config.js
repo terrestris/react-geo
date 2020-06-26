b99bcbbad7ff6568661f8b1a8a43d6c8b33904a6
Daniel Koch <koch@terrestris.de>
2020-06-26 13:16:50 +0200

    Add transform ignore pattern for required modules

--- jest.config.js
+++ jest.config.js
@@ -21,7 +21,7 @@ module.exports = {
     '<rootDir>/dist/'
   ],
   transformIgnorePatterns: [
-    'node_modules/(?!(ol|antd|(rc-[a-z-]*)|css-animation)/)'
+    'node_modules/(?!(ol|antd|(rc-[a-z-]*)|@ant-design\/css-animation|@babel\/runtime)/)'
   ],
   setupFiles: [
     '<rootDir>/jest/__mocks__/shim.js',
