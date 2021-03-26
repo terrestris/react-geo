a63667e8f9d969e47a91022dab9faa351cb8d90e
Simon Seyock <seyock@terrestris.de>
2021-03-26 12:03:50 +0100

    test: Use eslint plugins for testing library

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,13 @@
 module.exports = {
-  extends: '@terrestris/eslint-config-typescript',
+  extends: [
+    '@terrestris/eslint-config-typescript',
+    "plugin:testing-library/recommended",
+    "plugin:jest-dom/recommended"
+  ],
+  plugins: [
+    "testing-library",
+    "jest-dom"
+  ],
   rules: {
     'no-underscore-dangle': 'off'
   }
