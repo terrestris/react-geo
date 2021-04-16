0688e4ef5ba07c1a3ed22af5dd5ef27b40c13b33
Lukas Lohoff <lohoff@terrestris.de>
2021-04-16 17:50:31 +0200

    migrate layerTransparency tests

--- src/Slider/LayerTransparencySlider/LayerTransparencySlider.spec.tsx
+++ src/Slider/LayerTransparencySlider/LayerTransparencySlider.spec.tsx
@@ -1,11 +1,16 @@
 import TestUtil from '../../Util/TestUtil';
 import LayerTransparencySlider from './LayerTransparencySlider';
 
+import { render, screen, within } from '@testing-library/react';
+import * as React from 'react';
+import userEvent from '@testing-library/user-event';
+
 describe('<LayerTransparencySlider />', () => {
+
   let layer;
 
   beforeEach(() => {
-    layer = TestUtil.createVectorLayer();
+    layer = TestUtil.createVectorLayer({});
   });
 
   it('is defined', () => {
@@ -13,32 +18,26 @@ describe('<LayerTransparencySlider />', () => {
   });
 
   it('can be rendered', () => {
-    const props = {
-      layer: layer
-    };
-    const wrapper = TestUtil.mountComponent(LayerTransparencySlider, props);
-    expect(wrapper).not.toBeUndefined();
+    const { container } = render(<LayerTransparencySlider layer={layer} />);
+    expect(container).toBeVisible();
   });
 
   it('returns the the transparency of the layer', () => {
     layer.setOpacity(0.09);
-    const props = {
-      layer: layer
-    };
-
-    const wrapper = TestUtil.mountComponent(LayerTransparencySlider, props);
-    const transparency = wrapper.instance().getLayerTransparency();
-    expect(transparency).toBe(91);
+    render(<LayerTransparencySlider layer={layer} />);
+    const slider = screen.getByRole('slider');
+    expect(slider).toBeVisible();
+    expect(slider).toHaveAttribute('aria-valuenow', '91');
   });
 
   it('updates the opacity of the layer by providing a transparency value', () => {
-    const props = {
-      layer: layer
-    };
-    const wrapper = TestUtil.mountComponent(LayerTransparencySlider, props);
-
-    wrapper.instance().setLayerTransparency(91);
-    expect(layer.getOpacity()).toBe(0.09);
+    render(<LayerTransparencySlider layer={layer} value={91} />);
+    const slider = screen.getByRole('slider');
+    userEvent.click(slider);
+    userEvent.keyboard('{arrowright}'); // required to update opacity value
+    expect(slider).toBeVisible();
+    expect(slider).toHaveAttribute('aria-valuenow', '91');
+    expect(layer.getOpacity()).toBe(0.08);
   });
 
 });
