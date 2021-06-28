a7e4d3eceb3620600d347b0ec54f9afbdab790e6
Daniel Koch <koch@terrestris.de>
2021-06-28 15:24:13 +0200

    Fix eslint variable format name wanring

--- src/Util/antdTestQueries.ts
+++ src/Util/antdTestQueries.ts
@@ -1,28 +1,28 @@
 import { buildQueries, queryAllByTitle } from '@testing-library/react';
 
-function _queryAllAntdDropdownOption(container) {
+const allAntdDropdownOptionQuery = (container) => {
   return container.querySelectorAll('.ant-select-dropdown-option');
-}
+};
 
-function _queryAllAntdDropdownOptionByText(container, text: string) {
+const allAntdDropdownOptionByTextQuery = (container, text: string) => {
   const dropdowns = Array.from(container.querySelectorAll('.ant-select-dropdown'));
   const options = dropdowns.map(dropdown => queryAllByTitle(dropdown, text))
     .reduce((acc, val) => val !== null ? acc.concat(val) : acc, []);
   return options;
-}
+};
 
-const _multipleError = () => 'Found multiple antd dropdown options.';
+const multipleError = () => 'Found multiple antd dropdown options.';
 
-const _getMissingError = () => 'Unable to find antd dropdown option.';
+const missingError = () => 'Unable to find antd dropdown option.';
 
-function buildDocumentQueries(queryAll, multipleError, getMissingError) {
+const buildDocumentQueries = (queryAll, getMultipleError, getMissingError) => {
   const [
     query,
     getAll,
     get,
     findAll,
     find
-  ] = buildQueries(queryAll, multipleError, getMissingError);
+  ] = buildQueries(queryAll, getMultipleError, getMissingError);
   return [
     () => queryAll(document),
     () => query(document),
@@ -31,16 +31,16 @@ function buildDocumentQueries(queryAll, multipleError, getMissingError) {
     () => findAll(document, null),
     () => find(document, null)
   ];
-}
+};
 
-function buildDocumentByTextQueries(queryAll, multipleError, getMissingError) {
+const buildDocumentByTextQueries = (queryAll, getMultipleError, getMissingError) => {
   const [
     query,
     getAll,
     get,
     findAll,
     find
-  ] = buildQueries(queryAll, multipleError, getMissingError);
+  ] = buildQueries(queryAll, getMultipleError, getMissingError);
   return [
     (text: string) => queryAll(document, text),
     (text: string) => query(document, text),
@@ -49,7 +49,7 @@ function buildDocumentByTextQueries(queryAll, multipleError, getMissingError) {
     (text: string) => findAll(document, text),
     (text: string) => find(document, text)
   ];
-}
+};
 
 const [
   queryAllAntdDropdownOption,
@@ -58,7 +58,7 @@ const [
   getAntdDropdownOption,
   findAllAntdDropdownOption,
   findAntdDropdownOption
-] = buildDocumentQueries(_queryAllAntdDropdownOption, _multipleError, _getMissingError);
+] = buildDocumentQueries(allAntdDropdownOptionQuery, multipleError, missingError);
 
 const [
   queryAllAntdDropdownOptionByText,
@@ -67,7 +67,7 @@ const [
   getAntdDropdownOptionByText,
   findAllAntdDropdownOptionByText,
   findAntdDropdownOptionByText
-] = buildDocumentByTextQueries(_queryAllAntdDropdownOptionByText, _multipleError, _getMissingError);
+] = buildDocumentByTextQueries(allAntdDropdownOptionByTextQuery, multipleError, missingError);
 
 export {
   queryAllAntdDropdownOption,
