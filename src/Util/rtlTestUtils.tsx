f9fbcff0731c46df407fecc3b587438f4bde5b96
Simon Seyock <seyock@terrestris.de>
2021-08-27 16:03:11 +0200

    renamed test render function and make clickMap fire singleClick events

--- src/Util/rtlTestUtils.tsx
+++ src/Util/rtlTestUtils.tsx
@@ -11,30 +11,27 @@ export async function actSetTimeout(time: number): Promise<void> {
   });
 }
 
-/**
- * Be aware that this will only work if the map was initialized with `setSize` and `renderSync` after adding it to
- * the dom.
- * @param map
- * @param x
- * @param y
- */
-export function clickMap(map: OlMap, x: number, y: number) {
+function fireClickEvents(map: OlMap, x: number, y: number) {
   const pointerId = Math.random().toFixed(10).slice(2);
 
   const evt1 = new MouseEvent('pointerdown', {
     clientX: x,
-    clientY: y
+    clientY: y,
+    button: 0
   });
 
+  // @ts-ignore
   evt1.pointerId = pointerId;
 
   fireEvent(map.getViewport(), evt1);
 
   const evt2 = new MouseEvent('pointerup', {
     clientX: x,
-    clientY: y
+    clientY: y,
+    button: 0
   });
 
+  // @ts-ignore
   evt2.pointerId = pointerId;
 
   fireEvent(document, evt2);
@@ -47,25 +44,48 @@ export function clickMap(map: OlMap, x: number, y: number) {
  * @param x
  * @param y
  */
+export function clickMap(map: OlMap, x: number, y: number) {
+  jest.useFakeTimers();
+  fireClickEvents(map, x, y);
+  jest.runAllTimers();
+  jest.useRealTimers();
+}
+
+/**
+ * Be aware that this will only work if the map was initialized with `setSize` and `renderSync` after adding it to
+ * the dom.
+ * @param map
+ * @param x
+ * @param y
+ */
 export function doubleClickMap(map: OlMap, x: number, y: number) {
-  clickMap(map, x, y);
-  clickMap(map, x, y);
+  fireClickEvents(map, x, y);
+  fireClickEvents(map, x, y);
 }
 
 /**
  * This function renders the given element inside a map context and initializes the map with size `[400, 400]`, ready
  * to be used by the event functions in this file.
  */
-export function renderInContext(map: OlMap, element: ReactElement) {
-  const result = render(
-    <MapContext.Provider value={map}>
+export function renderInMapContext(map: OlMap, element: ReactElement, size: [number, number] = [400, 400]) {
+  const assemble = (element: ReactElement) => {
+    return <MapContext.Provider value={map}>
       <MapComponent map={map} />
       {element}
-    </MapContext.Provider>
-  );
+    </MapContext.Provider>;
+  }
+
+  const { rerender, ...results } = render(assemble(element));
 
   map.setSize([400, 400]);
   map.renderSync();
 
-  return result;
+  const rerenderInMapContext = (element: ReactElement) => {
+    rerender(assemble(element));
+  }
+
+  return {
+    rerenderInContext: rerenderInMapContext,
+    ...results
+  };
 }
