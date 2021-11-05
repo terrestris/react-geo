a982a83fa43c46da9ce887dae16f34824e10f138
Simon Seyock <seyock@terrestris.de>
2021-11-05 16:49:44 +0100

    feat: Add a CopyButton component

---
+++ src/Button/CopyButton/CopyButton.example.md
@@ -0,0 +1,69 @@
+This demonstrates the use of the CopyButton.
+
+```jsx
+import { useEffect, useState } from 'react';
+
+import OlMap from 'ol/Map';
+import OlView from 'ol/View';
+import OlLayerTile from 'ol/layer/Tile';
+import OlSourceOsm from 'ol/source/OSM';
+import OlFormatGeoJSON from 'ol/format/GeoJSON';
+import { fromLonLat } from 'ol/proj';
+
+import MapContext from '@terrestris/react-geo/Context/MapContext/MapContext'
+import MapComponent from '@terrestris/react-geo/Map/MapComponent/MapComponent';
+import CopyButton from '@terrestris/react-geo/Button/CopyButton/CopyButton';
+import { DigitizeUtil } from '@terrestris/react-geo/Util/DigitizeUtil';
+
+import federalStates from '../../../assets/federal-states-ger.json';
+
+const format = new OlFormatGeoJSON();
+const features = format.readFeatures(federalStates);
+
+const CopyButtonExample = () => {
+  const [map, setMap] = useState();
+
+  useEffect(() => {
+    const newMap = new OlMap({
+      layers: [
+        new OlLayerTile({
+          name: 'OSM',
+          source: new OlSourceOsm()
+        })
+      ],
+      view: new OlView({
+        center: fromLonLat([8, 50]),
+        zoom: 4
+      })
+    });
+
+    const digitizeLayer = DigitizeUtil.getDigitizeLayer(newMap);
+    digitizeLayer.getSource().addFeatures(features);
+
+    setMap(newMap);
+  }, []);
+
+  if (!map) {
+    return null;
+  }
+
+  return (
+    <div>
+      <MapContext.Provider value={map}>
+        <MapComponent
+          map={map}
+          style={{
+            height: '400px'
+          }}
+        />
+    
+        <CopyButton>
+          Copy feature
+        </CopyButton>
+      </MapContext.Provider>
+    </div>
+  );
+}
+
+<CopyButtonExample />
+```
---
+++ src/Button/CopyButton/CopyButton.tsx
@@ -0,0 +1,94 @@
+import * as React from 'react';
+import { useEffect, useState } from 'react';
+
+import OlVectorSource from 'ol/source/Vector';
+import OlGeometry from 'ol/geom/Geometry';
+import OlVectorLayer from 'ol/layer/Vector';
+
+import AnimateUtil from '@terrestris/ol-util/dist/AnimateUtil/AnimateUtil';
+
+import { CSS_PREFIX } from '../../constants';
+import { useMap } from '../../Hook/useMap';
+import { DigitizeUtil } from '../../Util/DigitizeUtil';
+import SelectFeaturesButton, { SelectFeaturesButtonProps } from '../SelectFeaturesButton/SelectFeaturesButton';
+import { SelectEvent } from 'ol/interaction/Select';
+
+interface OwnProps {
+  /**
+   * The vector layer which will be used for digitize features.
+   * The standard digitizeLayer can be retrieved via `DigitizeUtil.getDigitizeLayer(map)`.
+   */
+  digitizeLayer?: OlVectorLayer<OlVectorSource<OlGeometry>>;
+}
+
+export type CopyButtonProps = OwnProps & Omit<SelectFeaturesButtonProps, 'layers'>;
+
+/**
+ * The className added to this component.
+ */
+const defaultClassName = `${CSS_PREFIX}copybutton`;
+
+const CopyButton: React.FC<CopyButtonProps> = ({
+  className,
+  onFeatureSelect,
+  digitizeLayer,
+  ...passThroughProps
+}) => {
+
+  const [layers, setLayers] = useState<[OlVectorLayer<OlVectorSource<OlGeometry>>]>(null);
+
+  const map = useMap();
+
+  useEffect(() => {
+    if (!map) {
+      return;
+    }
+
+    if (digitizeLayer) {
+      setLayers([digitizeLayer]);
+    } else {
+      setLayers([DigitizeUtil.getDigitizeLayer(map)]);
+    }
+  }, [map, digitizeLayer]);
+
+  const onSelectInternal = (event: SelectEvent) => {
+    onFeatureSelect?.(event);
+
+    const feat = event.selected[0];
+
+    if (!feat) {
+      return;
+    }
+
+    const copy = feat.clone();
+
+    layers[0].getSource().addFeature(copy);
+
+    AnimateUtil.moveFeature(
+      map,
+      layers[0],
+      copy,
+      500,
+      50
+    );
+  };
+
+  const finalClassName = className
+    ? `${defaultClassName} ${className}`
+    : defaultClassName;
+
+  if (!layers) {
+    return null;
+  }
+
+  return <SelectFeaturesButton
+    layers={layers}
+    onFeatureSelect={onSelectInternal}
+    className={finalClassName}
+    clearAfterSelect={true}
+    {...passThroughProps}
+  />;
+
+};
+
+export default CopyButton;
