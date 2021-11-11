323de2fd2b95c17038b5c0b364acbf407769c204
Daniel Koch <koch@terrestris.de>
2021-11-11 16:38:20 +0100

    Combine imports for react

--- src/Button/CopyButton/CopyButton.tsx
+++ src/Button/CopyButton/CopyButton.tsx
@@ -1,5 +1,7 @@
-import * as React from 'react';
-import { useEffect, useState } from 'react';
+import React, {
+  useEffect,
+  useState
+} from 'react';
 
 import OlVectorSource from 'ol/source/Vector';
 import OlGeometry from 'ol/geom/Geometry';
