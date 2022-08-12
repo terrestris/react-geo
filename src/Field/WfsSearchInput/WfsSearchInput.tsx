c4cdca15949220e286ae64e43d5b86532691c8a9
Kai Volland <volland@terrestris.de>
2022-08-12 12:58:56 +0200

    feat: allow reading of GML32 responses

--- src/Field/WfsSearchInput/WfsSearchInput.tsx
+++ src/Field/WfsSearchInput/WfsSearchInput.tsx
@@ -9,6 +9,8 @@ import LoadingOutlined from '@ant-design/icons/LoadingOutlined';
 import CloseCircleOutlined from '@ant-design/icons/CloseCircleOutlined';
 
 import OlMap from 'ol/Map';
+import OlFormatGML32 from 'ol/format/GML32';
+import OlFormatGeoJson from 'ol/format/GeoJSON';
 
 import _debounce from 'lodash/debounce';
 
@@ -326,7 +328,18 @@ export class WfsSearchInput extends React.Component<WfsSearchInputProps, WfsSear
             body: new XMLSerializer().serializeToString(request),
             ...additionalFetchOptions
           });
-          this.onFetchSuccess(await response.json());
+          let data;
+          if (outputFormat === 'application/json' ) {
+            data = response.json();
+          } else {
+            const xml = response.text();
+            // TODO: Add support for other GML formats
+            const gmlParser = new OlFormatGML32();
+            const geojsonParser = new OlFormatGeoJson();
+            const features = gmlParser.readFeatures(xml);
+            data = geojsonParser.writeFeaturesObject(features);
+          }
+          this.onFetchSuccess(data);
         } catch (e) {
           this.onFetchError(e);
         }
