b9ad1ce4f18865be3c2de4b5dcdb13ca513e5364
Daniel Koch <koch@terrestris.de>
2023-09-04 21:25:25 +0200

    build: update enzyme adapter

--- jest/setup.js
+++ jest/setup.js
@@ -1,5 +1,5 @@
 import Enzyme from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
 import 'whatwg-fetch';
 import 'jest-canvas-mock';
 import '@testing-library/jest-dom';
