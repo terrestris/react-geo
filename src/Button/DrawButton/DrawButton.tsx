a0f50af0b4135805bd4971975952a1bab766dad8
Simon Seyock <seyock@terrestris.de>
2023-09-06 16:13:49 +0200

    refactor: `CopyButton`, `DeleteButton`, `SelectFeaturesButton` use hooks now

--- src/Button/DrawButton/DrawButton.tsx
+++ src/Button/DrawButton/DrawButton.tsx
@@ -43,7 +43,7 @@ interface OwnProps {
   modalPromptCancelButtonText?: string;
 }
 
-export type DrawButtonProps = OwnProps & Omit<UseDrawProps, 'drawType'> & Partial<ToggleButtonProps>;
+export type DrawButtonProps = OwnProps & Omit<UseDrawProps, 'drawType'|'active'> & Partial<ToggleButtonProps>;
 
 /**
  * The className added to this component.
