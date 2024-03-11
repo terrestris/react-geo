73d7980798c7eb68f9d8d4fa3d12eec06320bc86
Michael Holthausen <mholthausen@terrestris.de>
2024-03-11 13:01:00 +0100

    test: adds more tests for TimeSlider

--- src/Slider/TimeSlider/TimeSlider.spec.tsx
+++ src/Slider/TimeSlider/TimeSlider.spec.tsx
@@ -1,6 +1,8 @@
-import {
-  render
-} from '@testing-library/react';
+import '@testing-library/jest-dom/jest-globals';
+import '@testing-library/jest-dom';
+
+import { fireEvent, render, screen } from '@testing-library/react';
+import moment from 'moment';
 import React from 'react';
 
 import TimeSlider from './TimeSlider';
@@ -16,12 +18,113 @@ describe('<TimeSlider />', () => {
         defaultValue={''}
         min={''}
         max={''}
-        onChange={()=>{}}
+        onChange={() => {}}
         value={''}
         formatString={''}
         marks={undefined}
-      />);
+      />
+    );
     expect(container).toBeVisible();
   });
 
+  it('initializes and displays values correctly', () => {
+    const defaultValue = moment().subtract(30, 'minutes').toISOString();
+    const minValue = moment().subtract(1, 'hour').toISOString();
+    const maxValue = moment().toISOString();
+    const formatString = 'DD.MM.YYYY HH:mm';
+
+    render(
+      <TimeSlider
+        defaultValue={defaultValue}
+        min={minValue}
+        max={maxValue}
+        onChange={() => {}}
+        value={defaultValue}
+        formatString={formatString}
+        marks={undefined}
+      />
+    );
+
+    const slider = screen.getByRole('slider');
+    expect(slider).toHaveAttribute(
+      'aria-valuemin',
+      expect.stringContaining(moment(minValue).unix().toString())
+    );
+    expect(slider).toHaveAttribute(
+      'aria-valuemax',
+      expect.stringContaining(moment(maxValue).unix().toString())
+    );
+  });
+
+  it('calls onChange', () => {
+    const onChangeMock = jest.fn();
+    const defaultValue = moment().subtract(30, 'minutes').toISOString();
+
+    render(
+      <TimeSlider
+        defaultValue={defaultValue}
+        min={moment().subtract(1, 'hour').toISOString()}
+        max={moment().toISOString()}
+        onChange={onChangeMock}
+        value={defaultValue}
+        formatString={'DD.MM.YYYY HH:mm'}
+        marks={undefined}
+      />
+    );
+
+    const slider = screen.getByRole('slider');
+    fireEvent.mouseOver(slider);
+    fireEvent.mouseDown(slider);
+    fireEvent.mouseMove(slider, { clientX: 100 });
+    fireEvent.mouseUp(slider);
+
+    expect(onChangeMock).toHaveBeenCalled();
+  });
+
+  it('displays marks', () => {
+    const marks = {
+      [moment().subtract(1, 'hour').toISOString()]: '-1hr',
+      [moment().subtract(30, 'minutes').toISOString()]: '-30mins',
+      [moment().toISOString()]: 'Now'
+    };
+
+    render(
+      <TimeSlider
+        defaultValue={moment().subtract(30, 'minutes').toISOString()}
+        min={moment().subtract(1, 'hour').toISOString()}
+        max={moment().toISOString()}
+        onChange={() => {}}
+        value={moment().toISOString()}
+        formatString={'DD.MM.YYYY HH:mm'}
+        marks={marks}
+      />
+    );
+
+    expect(screen.getByText('-1hr')).toBeInTheDocument();
+    expect(screen.getByText('-30mins')).toBeInTheDocument();
+    expect(screen.getByText('Now')).toBeInTheDocument();
+  });
+
+  it('displays tooltip on hover', async () => {
+    const formatString = 'DD.MM.YYYY HH:mm';
+    const value = moment().subtract(30, 'minutes').unix();
+
+    render(
+      <TimeSlider
+        defaultValue={moment(value * 1000).toISOString()}
+        min={moment().subtract(1, 'hour').toISOString()}
+        max={moment().toISOString()}
+        onChange={() => {}}
+        value={moment(value * 1000).toISOString()}
+        formatString={formatString}
+        marks={undefined}
+      />
+    );
+
+    const slider = screen.getByRole('slider');
+    fireEvent.mouseOver(slider);
+    expect(
+      screen.getByText(moment(value * 1000).format(formatString))
+    ).toBeInTheDocument();
+  });
 });
