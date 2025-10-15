16af5aa645a9eaa71badd32758a7a114f32ed701
Marc Jansen <jansen@terrestris.de>
2025-10-15 12:33:45 +0200

    refactor: remove commented out code

--- src/Button/ToggleGroup/ToggleGroup.spec.tsx
+++ src/Button/ToggleGroup/ToggleGroup.spec.tsx
@@ -2,7 +2,6 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import * as React from 'react';
 
-// import TestUtil from '../../Util/TestUtil';
 import ToggleButton from '../ToggleButton/ToggleButton';
 import ToggleGroup from './ToggleGroup';
 
